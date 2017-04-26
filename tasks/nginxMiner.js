706ad2748cd8ccfe0edca84907983c05139b3c2d
zhuyali <495162889@qq.com>
2017-04-26 10:54:11 +0800

    fix promise

--- tasks/nginxMiner.js
+++ tasks/nginxMiner.js
@@ -12,6 +12,7 @@ const nginxModel = platformModel.nginx;
 const nginxServer = config.nginxServer;
 
 module.exports = function () {
+  console.log('nginxMiner start');
   let date = moment().format('YYYYMMDD');
   async.whilst(
     function () { return true; },
@@ -44,8 +45,7 @@ module.exports = function () {
               }
             } else {
               setTimeout(function () {
-                let flag = false;
-                let retry = 5;
+                let retry = 3;
                 let tempIntervalId = setInterval(function () {
                   pvModel
                     .getByDateAndDomainAndIp(date, domain, uriOrIp)
@@ -59,34 +59,36 @@ module.exports = function () {
                         clearInterval(tempIntervalId);
                       } else {
                         let province;
-                        let provinces = pvModel.getProvinceByIp(uriOrIp);
-                        if (provinces && provinces.length) {
-                          province = provinces[0];
-                          pvModel.add(date, domain, uriOrIp, val, province);
-                          clearInterval(tempIntervalId);
-                        } else if (retry <= 0) {
-                          clearInterval(tempIntervalId);
-                        } else {
-                          retry--;
-                          request(`http://ip.taobao.com/service/getIpInfo.php?ip=${uriOrIp}`, function (err, response, body) {
-                            if (body && body) {
-                              let result;
-                              try {
-                                result = JSON.parse(decodeURI(body));
-                              } catch (e) {
-                                result = {};
-                              }
-                              if (result.code === 0) {
-                                flag = true;
-                                clearInterval(tempIntervalId);
-                                if (result.data.country_id === 'CN') {
-                                  province = result.data.region;
-                                  pvModel.add(date, domain, uriOrIp, val, province);
+                        pvModel
+                          .getProvinceByIp(uriOrIp)
+                          .then(provinces => {
+                            if (provinces && provinces.length) {
+                              province = provinces[0].province;
+                              pvModel.add(date, domain, uriOrIp, val, province);
+                              clearInterval(tempIntervalId);
+                            } else if (retry <= 0) {
+                              clearInterval(tempIntervalId);
+                            } else {
+                              retry--;
+                              request(`http://ip.taobao.com/service/getIpInfo.php?ip=${uriOrIp}`, function (err, response, body) {
+                                if (body) {
+                                  let result;
+                                  try {
+                                    result = JSON.parse(decodeURI(body));
+                                  } catch (e) {
+                                    result = {};
+                                  }
+                                  if (result.code === 0) {
+                                    clearInterval(tempIntervalId);
+                                    if (result.data.country_id === 'CN') {
+                                      province = result.data.region;
+                                      pvModel.add(date, domain, uriOrIp, val, province);
+                                    }
+                                  }
                                 }
-                              }
+                              });
                             }
                           });
-                        }
                       }
                     })
                 }, 30 * 1000);
